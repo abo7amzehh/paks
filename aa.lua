6a45cfba44d787e2d13158b2910bf8689e85ac9f
abo7amzehh <33004775+abo7amzehh@users.noreply.github.com>
2020-04-15 23:27:56 +0300

    Update aa.lua

--- aa.lua
+++ aa.lua
@@ -34,29 +34,22 @@ end
 
 
 function saf()
-
-
+ gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("2.2958874e-41;16384D;16384D;16384D;16384D;16384D::24",gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0,-1)
+if gg.getResultCount() == 1 then
+gg.alert("❌ وقف لا تكمل")
+else
+gg.searchNumber("2.2958874e-41", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+n=gg.getResultCount()
+jz=gg.getResults(n)
+for i=1, n do
+gg.addListItems({[1] = {address =jz[i].address+100,flags = 4,freeze = true,value = 70003}})
 gg.clearResults()
-  gg.setRanges(gg.REGION_C_ALLOC)
-  gg.searchNumber("2.2958874e-41;16384D;16384D;16384D;16384D;16384D::24", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
-  if gg.getResultCount() == 0 then
-    gg.alert("❌ زقف لا تكمل")
-  else
-    gg.searchNumber("2.2958874e-41", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
-    resultsCount = gg.getResultCount()
-    results = gg.getResults(resultsCount)
-    for RS= 1, resultsCount do
-      gg.addListItems({
-        [1] = {
-          address = results[RS].address + 100,
-          flags = 4,
-          freeze = true,
-          value = 0
-        }
-      })
-     gg.alert("تم كسر الحمايه ... فجرهم 👹")
-    end
-  end
+
+end
+gg.alert("تم كسر الحمايه ... فجرهم 👹")
+end
 end
 
 function b() 
